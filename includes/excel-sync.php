c698c644f9e60ec544fd8ad3207ec31050702ef0
Aalves <aalves.host@gmail.com>
2026-04-16 00:40:11 -0300

    fix: garante inicializacao segura do cron e remove erro critico (v1.0.74)

--- includes/excel-sync.php
+++ includes/excel-sync.php
@@ -7,10 +7,11 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// 1. Agendar o Cron (5 minutos)
-if (!wp_next_scheduled('rene_survey_excel_sync_cron')) {
-    wp_schedule_event(time(), 'five_minutes', 'rene_survey_excel_sync_cron');
-}
+add_action('init', function() {
+    if (!wp_next_scheduled('rene_survey_excel_sync_cron')) {
+        wp_schedule_event(time(), 'five_minutes', 'rene_survey_excel_sync_cron');
+    }
+});
 
 add_filter('cron_schedules', function($schedules) {
     if (!isset($schedules['five_minutes'])) {
